59f906efc823f834e3d827099454dad9c3219c10
mauromotion <hello@mauromotion.com>
2025-03-10 18:32:51 +0000

    set up authentication with DRF

--- config/config/urls.py
+++ config/config/urls.py
@@ -14,9 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    re_path("login", views.login),
+    re_path("register", views.register),
+    re_path("test_token", views.test_token),
 ]
