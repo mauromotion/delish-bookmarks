989ee1397b832a966133ace938b9c090a36d729f
mauromotion <hello@mauromotion.com>
2025-03-17 17:56:36 +0000

    fixed login issue in browsable API

--- config/delish/urls.py
+++ config/delish/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import include, path
 
 from . import views
 
@@ -6,5 +6,6 @@ urlpatterns = [
     path("login", views.login),
     path("register", views.register),
     path("test_token", views.test_token),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
     path("bookmarks/", views.bookmark_list),
 ]
