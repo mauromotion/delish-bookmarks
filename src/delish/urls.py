ab41c7178e0ab9d09d5b58100821ecbbe9203b10
mauromotion <hello@mauromotion.com>
2025-03-22 15:07:31 +0000

    Replaced default token auth with djangorestframework-simplejwt implementation

--- src/delish/urls.py
+++ src/delish/urls.py
@@ -1,4 +1,5 @@
 from django.urls import include, path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from . import views
 
@@ -6,7 +7,8 @@ urlpatterns = [
     path("", views.api_root),
     path("login", views.login),
     path("register", views.register),
-    path("test_token", views.test_token),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh", TokenRefreshView.as_view(), name="token_refresh"),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
     path("bookmarks/", views.BookmarkListAPIView.as_view(), name="user-bookmarks"),
     path("bookmarks/<int:pk>", views.BookmarkDetailAPIView.as_view()),
