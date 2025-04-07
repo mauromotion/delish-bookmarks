3dd09d8edea8b92ee8cecc3b62cee1d135061cdb
mauromotion <hello@mauromotion.com>
2025-04-07 17:48:32 +0100

    (frontend) added filter to show read it later bookmarks

--- src/frontend/src/components/BookmarksList.jsx
+++ src/frontend/src/components/BookmarksList.jsx
@@ -7,40 +7,63 @@ import BookmarkCard from "./BookmarkCard";
 const BookmarksList = () => {
   const { authFetch } = useAuth();
   const [bookmarks, setBookmarks] = useState([]);
+  const [tab, setTab] = useState("bookmarks");
 
-  // Fetch all bookmarks of the current user
-
-  // TODO: add query parameters to fetch filtered by collection and tags
-  useEffect(() => {
-    async function fetchBookmarks() {
-      const response = await authFetch(
-        "http://localhost:8000/api/bookmarks?q=all",
-        {
-          headers: {
-            "Content-type": "application/json",
-          },
+  async function fetchBookmarks(query) {
+    const response = await authFetch(
+      `http://localhost:8000/api/bookmarks${query}`,
+      {
+        headers: {
+          "Content-type": "application/json",
         },
+      },
+    );
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(
+        "Fetching bookmakrs failed: ",
+        errorData.detail || response.statusText,
       );
-
-      if (!response.ok) {
-        const errorData = await response.json();
-        throw new Error(
-          "Fetching bookmakrs failed: ",
-          errorData.detail || response.statusText,
-        );
-      }
-
-      const data = await response.json();
-      setBookmarks(data.results);
     }
 
-    fetchBookmarks();
+    const data = await response.json();
+    setBookmarks(data.results);
+  }
+
+  const handleReadItLaterTabClick = () => {
+    setTab("readItLater");
+    fetchBookmarks("?is_unread=True");
+  };
+
+  const handleBookmarksTabClick = () => {
+    setTab("bookmarks");
+    fetchBookmarks("?q=all");
+  };
+
+  // TODO: Extrapolate the fetchBookmarks(query) function
+  // TODO: add query parameters to fetch filtered by collection and tags
+  // Fetch all bookmarks of the current user at component mount
+  useEffect(() => {
+    fetchBookmarks("?q=all");
   }, []);
 
-  // TODO: add a "Read it Later" header that filters the bookmarks with a query
   return (
     <div className={classes.bmList}>
-      <h4>Bookmarks</h4>
+      <div className={classes.tabs}>
+        <h4
+          className={`${classes.label} ${tab === "bookmarks" ? classes.isActive : classes.isNotActive}`}
+          onClick={handleBookmarksTabClick}
+        >
+          Bookmarks
+        </h4>
+        <h4
+          className={`${classes.label} ${tab === "readItLater" ? classes.isActive : classes.isNotActive}`}
+          onClick={handleReadItLaterTabClick}
+        >
+          Read It Later
+        </h4>
+      </div>
       <div className={classes.list}>
         <ul>
           {bookmarks.map((bm) => (
--- src/frontend/src/store/auth-context.jsx
+++ src/frontend/src/store/auth-context.jsx
@@ -1,5 +1,5 @@
 import { jwtDecode } from "jwt-decode";
-import { useState, createContext } from "react";
+import { useState, useCallback, createContext } from "react";
 const AuthContext = createContext(undefined);
 
 export const AuthProvider = ({ children }) => {
@@ -101,7 +101,7 @@ export const AuthProvider = ({ children }) => {
   };
 
   // Refresh the access token
-  const refresh = async () => {
+  const refresh = useCallback(async () => {
     try {
       const response = await fetch("http://localhost:8000/api/token/refresh", {
         method: "POST",
@@ -128,7 +128,7 @@ export const AuthProvider = ({ children }) => {
     } catch (error) {
       console.log("Error: ", error);
     }
-  };
+  }, []);
 
   // Fetch any endpoint including the access token, try to refresh the token if error 401
   const authFetch = async (url, options = {}) => {
